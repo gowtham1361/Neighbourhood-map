c40f3da4dc178095364fcbd72dac3cc9836dd9c1
gowji <gowthamrgo@gmail.com>
2016-12-31 12:57:23 +0530

    added list search functionality
    yet to add the marker list functionality

--- js/map.js
+++ js/map.js
@@ -77,103 +77,110 @@ var map;
 var markers = [];
 
 function initMap() {
-  // Constructor creates a new map - only center and zoom are required.
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {
-      lat: 40.7413549,
-      lng: -73.9980244
-    },
-    zoom: 13
-  });
+    // Constructor creates a new map - only center and zoom are required.
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {
+            lat: 40.7413549,
+            lng: -73.9980244
+        },
+        zoom: 13
+    });
 
 
-  var largeInfowindow = new google.maps.InfoWindow();
-  var bounds = new google.maps.LatLngBounds();
+    var largeInfowindow = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
 
-  // The following group uses the location array to create an array of markers on initialize.
-  for (var i = 0; i < locations.length; i++) {
-    // Get the position from the location array.
-    var position = locations[i].location;
-    var title = locations[i].title;
-    // Create a marker per location, and put into markers array.
-    var marker = new google.maps.Marker({
-      map: map,
-      position: position,
-      title: title,
-      animation: google.maps.Animation.DROP,
-      id: i
-    });
-    // Push the marker to our array of markers.
-    markers.push(marker);
-    // Create an onclick event to open an infowindow at each marker.
-    marker.addListener('click', function() {
-      populateInfoWindow(this, largeInfowindow);
-    });
-    marker.addListener('click', function () {
-      toggleBounce(this);
-    });
-    bounds.extend(markers[i].position);
-  }
-  // Extend the boundaries of the map for each marker
-  map.fitBounds(bounds);
+    // The following group uses the location array to create an array of markers on initialize.
+    for (var i = 0; i < locations.length; i++) {
+        // Get the position from the location array.
+        var position = locations[i].location;
+        var title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        var marker = new google.maps.Marker({
+            map: map,
+            position: position,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i
+        });
+        // Push the marker to our array of markers.
+        markers.push(marker);
+        // Create an onclick event to open an infowindow at each marker.
+        marker.addListener('click', function() {
+            populateInfoWindow(this, largeInfowindow);
+        });
+        marker.addListener('click', function() {
+            toggleBounce(this);
+        });
+        bounds.extend(markers[i].position);
+    }
+    // Extend the boundaries of the map for each marker
+    map.fitBounds(bounds);
 }
 
 // This function populates the infowindow when the marker is clicked. We'll only allow
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
-  // Check to make sure the infowindow is not already opened on this marker.
-  if (infowindow.marker != marker) {
-    infowindow.marker = marker;
-    infowindow.setContent('<div>' + marker.title + '</div>');
-    infowindow.open(map, marker);
-    // Make sure the marker property is cleared if the infowindow is closed.
-   // infowindow.addListener('closeclick', function() {
-     // infowindow.setMarker(null);
-    //});
-  }
+    // Check to make sure the infowindow is not already opened on this marker.
+    if (infowindow.marker != marker) {
+        infowindow.marker = marker;
+        infowindow.setContent('<div>' + marker.title + '</div>');
+        infowindow.open(map, marker);
+        // Make sure the marker property is cleared if the infowindow is closed.
+        // infowindow.addListener('closeclick', function() {
+        // infowindow.setMarker(null);
+        //});
+    }
 }
 //multiple markers are bouncing when selected.
 //adding bounce functionality to the marker when clicked.
 // code obtained from https://developers.google.com/maps/documentation/javascript/examples/marker-animations
 function toggleBounce(marker) {
-        if (marker.getAnimation() !== null) {
-          marker.setAnimation(null);
-        } else {
-          marker.setAnimation(google.maps.Animation.BOUNCE);
-        }
-      }
+    if (marker.getAnimation() !== null) {
+        marker.setAnimation(null);
+    } else {
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+    }
+}
+
+//Location constructor similar to Cat constructor
+var Location = function(locationData) {
+    this.title = ko.observable(locationData.title);
+    this.lat = ko.observable(locationData.location.lat);
+    this.lng = ko.observable(locationData.location.lng);
+};
 
 
 // Constructor
 // Function expression
 var ViewModel = function() {
-  var self = this;
-  this.locationsList = ko.observableArray(locations);
-  this.inputItem = ko.observable('');
-  console.log(this.inputItem);
-//used live search code from http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
-  this.searchFilter= function (value){
-    console.log('searchFilter running ');
-    //remove all the location from the list
-    self.locationsList.removeAll();
-    console.log(value);
-    //value is the input from the textInput
-    //now we need to add the filtering
-    var loopList = self.locationsList;
-    for(var y in loopList) {
-      console.log('for loop working');
-      //add the logic
-      //if(beers[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
-      //  viewModel.beers.push(beers[x]);
-      //}
-      if(loopList[y].title.toLowerCase().indexOf(value.toLowercase()) >= 0) {
-              console.log('if loop working');
-        self.loopList.push(loopList[y]);
-      }
-    }
+    var self = this;
+    this.locationsList = ko.observableArray([]);
+    //using the Location constructor similar to the Cat constructor  in adding more cats video and pushing to the locationList array
+    locations.forEach(function(item) {
+        self.locationsList.push(new Location(item));
+    });
+    // setting up a observable to  be notified by the  input search box.
+    this.inputItem = ko.observable('');
+    //used live search code from http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
+    this.searchFilter = function(value) {
+        console.log('searchFilter running ');
+        //remove all the location from the list
+        self.locationsList.removeAll();
+        console.log(value);
+        //value is the input from the textInput
+        //now we need to add the filtering
+        //here the locations is the hardcoded list on top of map.js
+        for (var y in locations) {
+            //add the logic finding whether the indexof() returns a matching value in the looplist array.
+            if (locations[y].title.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                //pushing the matching locations to the self.locationsList ko.observableArray
+                self.locationsList.push(locations[y]);
+            }
+        }
 
-  };
+    };
 };
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/new
