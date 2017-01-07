119b0a99b8049afdd210a8ab7df21a9d8ffe9c48
gowji <gowthamrgo@gmail.com>
2017-01-07 20:33:07 +0530

    working ajax version
    error handling not working

--- js/map.js
+++ js/map.js
@@ -1,4 +1,5 @@
 //TODO:
+//error handlings to be taken care of.
 //https://api.foursquare.com/v2/venues/search?ll=12.955155,77.585466&client_id=JEKKICPOXJXLKTOYHA535NWKZK35SI31G2ILXD42IU0ETRPM&client_secret=5UKJWXIY44IKDUVU43WK4VYXBKXNRHKNVTMLYQAB0IDOD3DP&v=20131016
 // These are some of the locations of bangalore that will be shown to the user.
 //declaring a global object to store the instance of ViewModel
@@ -7,73 +8,65 @@ var appvm;
 var locations = [{
         title: 'The Forum',
         location: {
-            lat:  12.934545,
+            lat: 12.934545,
             lng: 77.611308
-        },
-        phone: 'nill'
+        }
     },
     {
         title: 'MTR Hotel',
         location: {
             lat: 12.955155,
             lng: 77.585466
-        },
-        phone: 'nill'
+        }
     },
     {
         title: ' Truffles',
         location: {
             lat: 12.971856,
             lng: 77.60095
-        },
-        phone: 'nill'
+        }
     },
     {
         title: 'Orion Mall',
         location: {
             lat: 13.010788,
             lng: 77.554902
-        },
-        phone: 'nill'
+        }
     },
     {
         title: 'Mantri Square Mall',
         location: {
             lat: 12.991723,
             lng: 77.570553
-        },
-        phone:'nill'
+        }
     },
     {
         title: 'Phoenix Marketcity',
         location: {
             lat: 12.997128,
             lng: 77.696323
-        },
-        phone:'nill'
+        }
     },
     {
         title: 'M Chinnaswamy Stadium',
         location: {
             lat: 12.97883,
             lng: 77.599615
-        },
-        phone:'nill'
+        }
     },
     {
         title: ' UB city',
         location: {
             lat: 12.971508,
             lng: 77.596409
-        },
-        phone:''
+        }
     },
 ];
 
 //foursquare api credentials
-var four_squareApi= {
-    ID : 'JEKKICPOXJXLKTOYHA535NWKZK35SI31G2ILXD42IU0ETRPM',
-    SECRET:'5UKJWXIY44IKDUVU43WK4VYXBKXNRHKNVTMLYQAB0IDOD3DP'
+var four_squareApi = {
+    ID: 'JEKKICPOXJXLKTOYHA535NWKZK35SI31G2ILXD42IU0ETRPM',
+    SECRET: '5UKJWXIY44IKDUVU43WK4VYXBKXNRHKNVTMLYQAB0IDOD3DP'
 };
 
 
@@ -99,21 +92,8 @@ function initMap() {
 
 
     // The following group uses the location array to create an array of markers on initialize.
-    for (var i = 0; i < locations.length; i++) {
 
-         //retrieving contact no of the place via foursquare api.
-        //how to add the phone no to the infowindow .
-        var url ='https://api.foursquare.com/v2/venues/search?' + 'll='+ locations[i].location.lat + ',' + locations[i].location.lng +'&client_id=' + four_squareApi.ID + '&client_secret=' +  four_squareApi.SECRET +  '&v=20131016';
-        $.getJSON(url , function(data) {
-            console.log(data);
-            //storing the  phone number in phoneNo variable
-              var phoneNo = data.response.venues[0].contact.phone;
-              locations[i].phone = phoneNo;
-              console.log('location phone', locations[i].phone)
-            console.log('phone :', phoneNo);//'location phone: ',locations[i]
-        }).fail(function() {
-            console.log('fail');
-            });
+    for (var i = 0; i < locations.length; i++) {
 
         // Get the position from the location array.
         var position = locations[i].location;
@@ -145,24 +125,44 @@ function initMap() {
 // one infowindow which will open at the marker that is clicked, and populate based
 // on that markers position.
 function populateInfoWindow(marker, infowindow) {
-    // Check to make sure the infowindow is not already opened on this marker.
-    for (var i = 0; i < locations.length; i++){
+    //using the ajax request here to be able to add infowindow content when any location is clicked from the list or the marker.
+    //using the id of the marker to access the corresponding location element.
+    var index = marker.id;
+    var location = locations[index];
+    var phoneNo;
+    var url = 'https://api.foursquare.com/v2/venues/search?' + 'll=' + locations[index].location.lat + ',' + locations[index].location.lng + '&client_id=' + four_squareApi.ID + '&client_secret=' + four_squareApi.SECRET + '&v=20131016';
 
-    }
+    $.getJSON(url, function(data) {
+        //storing the  phone number in phoneNo variable
+        phoneNo = data.response.venues[0].contact.phone;
+        infowindow.setContent('<div>' + '<b>' + marker.title + '</b>' + '</div>' +
+            '<div>' + 'phone: ' + phoneNo + '</div>' +
+            '<div>' + 'powered by ' + '<b>' + ' Foursquare' + '</b>' + '</div>');
+
+    }).fail(function(err) {
+        //this part is not working when the response fails
+        console.log('fail');
+        infowindow.setContent('<div>' + marker.title + '</div>' +
+            '<div>' + 'phone:' + phoneNo + ' failed to access foursquare :(' + '</div>' +
+            '<div>' + 'powered by' + '<b>' + ' ' + ' Foursquare' + '</b>' + '</div>');
+    });
+    // Check to make sure the infowindow is not already opened on this marker.
     if (infowindow.marker != marker) {
         infowindow.marker = marker;
         infowindow.setContent('<div>' + marker.title + '</div>');
         infowindow.open(map, marker);
-        // Make sure the marker property is cleared if the infowindow is closed.
-        // infowindow.addListener('closeclick', function() {
-        // infowindow.setMarker(null);
+        //Make sure the marker property is cleared if the infowindow is closed.
+        //infowindow.addListener('closeclick', function() {
+        //infowindow.setMarker(null);
         //});
     }
 }
+
 //multiple markers are bouncing when selected.
 //adding bounce functionality to the marker when clicked.
 // code obtained from https://developers.google.com/maps/documentation/javascript/examples/marker-animations
 function toggleBounce(marker) {
+
     if (marker.getAnimation() !== null) {
         marker.setAnimation(null);
     } else {
@@ -176,10 +176,10 @@ function toggleBounce(marker) {
 
 //Location constructor similar to Cat constructor
 var Location = function(locationData, id) {
+
     this.title = locationData.title;
     this.lat = locationData.location.lat;
     this.lng = locationData.location.lng;
-    this.phone = locationData.location.phone;
     this.id = id;
 };
 
@@ -200,7 +200,7 @@ var ViewModel = function() {
     this.inputItem = ko.observable('');
     //used live search code from http://opensoul.org/2011/06/23/live-search-with-knockoutjs/
     this.searchFilter = function(value) {
-      //  console.log('searchFilter running');
+        //  console.log('searchFilter running');
         //remove all the location from the list
         self.locationsList.removeAll();
         //now we need to add the filtering
@@ -218,6 +218,7 @@ var ViewModel = function() {
     //add marker filtering here
     //similar to the searchFilter function
     this.markerFilterfn = function(value) {
+
         for (var x in markers) {
             //set map for markers as null
             if (markers[x].setMap(map) !== null) {
@@ -233,6 +234,7 @@ var ViewModel = function() {
     //adding the displayInfoBounce function when the list is clicked
     //use the id of the clickedItem to access the corresponding marker and trigger the click event for the corresponding marker
     this.displayInfoBounce = function(clickedItem) {
+
         var index = clickedItem.id;
         var marker = markers[index];
         google.maps.event.trigger(marker, 'click');
@@ -246,8 +248,7 @@ appvm = new ViewModel();
 
 // Activate Knockout
 ko.applyBindings(appvm);
-//moved to the map init function
+
 //using subscribe method of ko to the observalble appvm.inputItem
 appvm.inputItem.subscribe(appvm.searchFilter);
 appvm.inputItem.subscribe(appvm.markerFilterfn);
-
